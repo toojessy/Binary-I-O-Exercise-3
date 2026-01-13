2a48f2850cbf6b1dbc5516fe0c53fca056274d33
toojessy <jnkamienski@my.waketech.edu>
2026-01-13 16:22:25 -0500

    Update Exercise17_03.java

--- Exercise17_03.java
+++ Exercise17_03.java
@@ -2,48 +2,49 @@ import java.io.*;
 
 /**
  * Exercise 3: Track how many times a program has been executed
- *
- * Requirements:
- * - Store an execution count in a file named Exercise17_03.dat
- * - Each time this program runs, increment the count by 1
- * - Display the current count
- * - Use the file persistence pattern:
- *   1. Read existing count (if file exists)
- *   2. Increment the count
- *   3. Write updated count back to file
- *
- * @author Your Name
  */
 public class Exercise17_03 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        File file = new File("Exercise17_03.dat");
+        File file = new File(System.getProperty("user.home"), "Exercise17_03.dat");
         int count = 0;
 
         // STEP 1: Read existing count if file exists
         // TODO: Check if file exists using file.exists()
+        if (file.exists()) {
 
+            // TODO: If file exists, open DataInputStream and read the count
+            try (DataInputStream input =
+                         new DataInputStream(new FileInputStream(file))) {
+                count = input.readInt();
 
-        // TODO: If file exists, open DataInputStream and read the count
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-
-        // TODO: Close the input stream (or use try-with-resources)
-
+            // TODO: Close the input stream (or use try-with-resources)
+            // (Handled automatically)
+        }
 
         // STEP 2: Increment the count
         // TODO: Add 1 to count
-
+        count++;
 
         // STEP 3: Write updated count back to file
         // TODO: Create DataOutputStream for the file
+        try (DataOutputStream output =
+                     new DataOutputStream(new FileOutputStream(file))) {
 
+            // TODO: Write the updated count
+            output.writeInt(count);
 
-        // TODO: Write the updated count
+            // TODO: Close the output stream (or use try-with-resources)
+            // (Handled automatically)
 
-
-        // TODO: Close the output stream (or use try-with-resources)
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Display the result
         System.out.println("This program has been executed " + count + " time(s).");
